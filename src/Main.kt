e08b927415d2b7dc474938b3dc99536c076a1f25
ErfanRad <raderfann@gmail.com>
2025-12-03 22:18:01 -0800

    haleDovomin Tamrin leetcode

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,21 @@
 class Solution {
-    fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
-        var p1 = m - 1
-        var p2 = n - 1
-        var p = m + n - 1
+    fun removeElement(nums: IntArray, `vall`: Int): Int {
 
-        while (p2 >= 0) {
-            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
-                nums1[p] = nums1[p1]
-                p1--
-            } else {
-                nums1[p] = nums2[p2]
-                p2--
+        //[1,5,7,3,9]
+
+        val sizeNums = nums.size // 4
+
+        var k = 0
+
+        for ( i in 0 until sizeNums ) {
+            if(nums[i] != vall ){
+                nums[k] = nums[i]
+                k++
+
             }
-            p--
         }
+
+        return k
+
     }
 }

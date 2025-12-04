6d33c296905bdc178308a652ad798fc072340f89
ErfanRad <raderfann@gmail.com>
2025-12-04 01:51:59 -0800

    haleChaharominsoaleLeetCode

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,16 @@
 class Solution {
     fun removeDuplicates(nums: IntArray): Int {
 
-        var k = 1
-     for (i in 1 until nums.size - 1) {
-         if (nums[i] != nums[i - 1]) {
-             nums[k] = nums[i]
-             k++
-         }
-     }
+        if (nums.size == 1 || nums.size == 0) {
+            return nums.size
+        }
+        var k = 2
+        for (i in 2 until nums.size) {
+            if (nums[i] != nums[k - 2]) {
+                nums[k] = nums[i]
+                k++
+            }
+        }
         return k
     }
 }

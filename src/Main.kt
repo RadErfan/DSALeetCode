5c687a403cd09dc57c04bc45ba58c87e78ae06f7
ErfanRad <raderfann@gmail.com>
2025-12-05 01:41:01 -0800

    hale soale 6 leetcode

--- src/Main.kt
+++ src/Main.kt
@@ -1,27 +1,25 @@
 class Solution {
-    fun majorityElement(nums: IntArray): Int {
+    fun rotate(nums: IntArray, k: Int): Unit {
+        val n = nums.size
+        val charkhesh = k % n
 
+        reverse(nums, 0, n - 1)
 
+        reverse(nums, 0, charkhesh - 1)
 
-      var  padeshah = nums[0]
-      var  count = 1
+        reverse(nums, charkhesh, n - 1)
+    }
 
-        for (i in 1 until nums.size) {
-            if (nums[i]!=padeshah) {
-                count--
-            }else{
-                count++
-            }
-            if (count ==0){
-                padeshah = nums[i]
-                count = 1
-            }
+    fun reverse(nums: IntArray, start: Int, end: Int) {
+        var left = start
+        var right = end
+        while (left < right) {
+            val temp = nums[left]
+            nums[left] = nums[right]
+            nums[right] = temp
 
-
-
-
-
+            left++
+            right--
         }
-        return padeshah
     }
 }

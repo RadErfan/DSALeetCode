da18d10f9e90d6362439ed2dc459653a61632e2a
ErfanRad <raderfann@gmail.com>
2025-12-05 02:29:47 -0800

    hale soale 7 leetcode

--- src/Main.kt
+++ src/Main.kt
@@ -1,25 +1,19 @@
+import kotlin.math.min
+
 class Solution {
-    fun rotate(nums: IntArray, k: Int): Unit {
-        val n = nums.size
-        val charkhesh = k % n
+    fun maxProfit(prices: IntArray): Int {
+       var minPrice = Int.MAX_VALUE
+        var maxPrice = 0
+        minPrice = prices[0]
+        for (i in prices.indices) {
 
-        reverse(nums, 0, n - 1)
-
-        reverse(nums, 0, charkhesh - 1)
-
-        reverse(nums, charkhesh, n - 1)
-    }
-
-    fun reverse(nums: IntArray, start: Int, end: Int) {
-        var left = start
-        var right = end
-        while (left < right) {
-            val temp = nums[left]
-            nums[left] = nums[right]
-            nums[right] = temp
-
-            left++
-            right--
+            if (prices[i] < minPrice) {
+                minPrice = prices[i]
+            }else if (prices[i]- minPrice > maxPrice) {
+                maxPrice = prices[i] - minPrice
+            }
         }
+
+        return maxPrice
     }
 }
